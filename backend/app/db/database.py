06744b2f51d16019f85a8371ae3ff0c974086751
Bethany <bethany.abebe@gmail.com>
2026-03-25 13:42:47 -0400

    feat: configure SQLAlchemy db connection and add session dep

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = 'postgresql://postgres:12345abcde!!@localhost:5432/rental_db'
+#TOD0: store in .env
+DATABASE_URL = 'postgresql://postgres:12345abcde!!@localhost:5432/rental_db' 
 
 engine = create_engine(DATABASE_URL)
 
